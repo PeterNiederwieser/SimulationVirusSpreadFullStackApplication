e6092697e924b84ba1946b6a1b62fd33b48b8160
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-11 08:43:01 +0200

    Add submit button with post request in Simulation.jsx

--- Frontend/src/ui/Simulation.jsx
+++ Frontend/src/ui/Simulation.jsx
@@ -1,5 +1,6 @@
 import {useState} from "react";
-import FormInput from "./components/FormInput.jsx";
+import FormSimulationParameters from "./components/FormSimulationParameters.jsx";
+import {postSimulationParameters} from "../service/requestMethods.js";
 
 function Simulation() {
     function initFormObject() {
@@ -23,41 +24,24 @@ function Simulation() {
         });
     }
 
+    function onSumbit(formObject) {
+        postSimulationParameters(formObject)
+            .then(() => {
+                console.log("parameters: ", formObject);
+            })
+            .catch(error => {
+                throw error;
+            });
+    }
+
     return (
         <>
-            <div>Enter the key data for your simulation: </div>
-            <div className="form">
-                <FormInput
-                    updateFormObject = {updateFormObject}
-                    value={formObject.simulationName}
-                    name="simulationName"
-                    placeholder="Simulation Name"
-                />
-                <FormInput
-                    updateFormObject = {updateFormObject}
-                    value={formObject.numberOfAnimals}
-                    name="numberOfAnimals"
-                    placeholder="Number of Animals (100 to 1000)"
-                />
-                <FormInput
-                    updateFormObject = {updateFormObject}
-                    value={formObject.numberOfInitialInfections}
-                    name="numberOfInitialInfections"
-                    placeholder="Number of initial infections"
-                />
-                <FormInput
-                    updateFormObject = {updateFormObject}
-                    value={formObject.virusInfectiousness}
-                    name="virusInfectiousness"
-                    placeholder="Virus infectiousness (0 to 100)"
-                />
-                <FormInput
-                    updateFormObject = {updateFormObject}
-                    value={formObject.mortalityRate}
-                    name="mortalityRate"
-                    placeholder="Mortality rate (0 to 100)"
-                />
-            </div>
+            <div>Enter the key data for your simulation:</div>
+            <FormSimulationParameters
+                updateFormObject={updateFormObject}
+                formObject={formObject}
+            />
+            <button className="form-button" onClick={() => onSumbit(formObject)}>Submit</button>
         </>
     )
 }
