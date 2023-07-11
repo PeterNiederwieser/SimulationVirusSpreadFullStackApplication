97e99e408d12734336c3ef6cfb4a3c3638e57bca
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-11 14:12:11 +0200

    Add further annotations to entity SimulationBasicData

--- Backend/src/main/java/com/example/Backend/persistence/entity/SimulationBasicData.java
+++ Backend/src/main/java/com/example/Backend/persistence/entity/SimulationBasicData.java
@@ -3,10 +3,16 @@ package com.example.Backend.persistence.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SimulationBasicData {
     @Id
     @GeneratedValue
