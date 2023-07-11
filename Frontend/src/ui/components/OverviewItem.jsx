9a7bad83e9234ffda32a870737bc56e05be66d00
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-11 18:25:09 +0200

    Add component OverviewItem.jsx

---
+++ Frontend/src/ui/components/OverviewItem.jsx
@@ -0,0 +1,10 @@
+function OverviewItem({item}) {
+    return (
+        <div className="overview-item">
+            <div className="overview-item-name">{item.simulationName}</div>
+        </div>
+    )
+}
+
+export default OverviewItem
+
