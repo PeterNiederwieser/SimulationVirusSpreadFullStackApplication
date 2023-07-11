38e40b1069effc0bd2243f27cf5ff5d4103bd3df
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-11 18:24:40 +0200

    Add submit button in component FormSimulationParameters

--- Frontend/src/ui/components/FormSimulationParameters.jsx
+++ Frontend/src/ui/components/FormSimulationParameters.jsx
@@ -1,43 +1,45 @@
 import FormInput from "./FormInput.jsx";
 
-function FormSimulationParameters({updateFormObject, formObject}) {
+function FormSimulationParameters({updateFormObject, formObject, onSubmit}) {
     return (
         <div className="form">
             <FormInput
-                updateFormObject = {updateFormObject}
+                updateFormObject={updateFormObject}
                 value={formObject.simulationName}
                 name="simulationName"
                 placeholder="Simulation Name"
             />
             <FormInput
-                updateFormObject = {updateFormObject}
+                updateFormObject={updateFormObject}
                 value={formObject.numberOfAnimals}
                 name="numberOfAnimals"
                 placeholder="Number of Animals (100 to 1000)"
             />
             <FormInput
-                updateFormObject = {updateFormObject}
+                updateFormObject={updateFormObject}
                 value={formObject.numberOfInitialInfections}
                 name="numberOfInitialInfections"
                 placeholder="Number of initial infections"
             />
             <FormInput
-                updateFormObject = {updateFormObject}
+                updateFormObject={updateFormObject}
                 value={formObject.virusInfectiousness}
                 name="virusInfectiousness"
                 placeholder="Virus infectiousness (0 to 100)"
             />
             <FormInput
-                updateFormObject = {updateFormObject}
+                updateFormObject={updateFormObject}
                 value={formObject.mortalityRate}
                 name="mortalityRate"
                 placeholder="Mortality rate (0 to 100)"
             />
+            <button className="form-button" onClick={() => onSubmit(formObject)}>Submit</button>
         </div>
     )
 }
 
 export default FormSimulationParameters
+
 
 
 
