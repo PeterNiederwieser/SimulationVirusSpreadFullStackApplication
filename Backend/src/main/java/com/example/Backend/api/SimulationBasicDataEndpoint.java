54d8058b523772abc6db9b207273239ff43bf441
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-11 09:54:33 +0200

    Add endpoints for CRUD operations in SimulationBasicDataEndpoint

--- Backend/src/main/java/com/example/Backend/api/SimulationBasicDataEndpoint.java
+++ Backend/src/main/java/com/example/Backend/api/SimulationBasicDataEndpoint.java
@@ -1,9 +1,10 @@
 package com.example.Backend.api;
 
+import com.example.Backend.persistence.entity.SimulationBasicData;
 import com.example.Backend.service.SimulationBasicDataService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("simulation-basic-data")
@@ -13,5 +14,31 @@ public class SimulationBasicDataEndpoint {
 
     public SimulationBasicDataEndpoint(SimulationBasicDataService simulationBasicDataService) {
         this.simulationBasicDataService = simulationBasicDataService;
+    }
+
+    @GetMapping
+    public List<SimulationBasicData> get() {
+        return simulationBasicDataService.findAll();
+    }
+
+    @GetMapping("{id}")
+    public SimulationBasicData getOne(@PathVariable long id) throws ElementNotFoundException {
+        return simulationBasicDataService.findById(id)
+                .orElseThrow(ElementNotFoundException::new);
+    }
+
+    @PostMapping
+    SimulationBasicData save(@RequestBody SimulationBasicData simulationBasicData) {
+        return simulationBasicDataService.save(simulationBasicData);
+    }
+
+    @PutMapping
+    SimulationBasicData update(@RequestBody SimulationBasicData simulationBasicData) {
+        return simulationBasicDataService.save(simulationBasicData);
+    }
+
+    @DeleteMapping("{id}")
+    void deleteOne(@PathVariable long id) {
+        simulationBasicDataService.deleteById(id);
     }
 }
