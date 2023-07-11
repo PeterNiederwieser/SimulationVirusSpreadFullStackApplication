7c7483162fc07f04344abeea1701d6a55a36bd40
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-11 17:45:55 +0200

    Add further methods for CRUD operations in requestMethods.js

--- Frontend/src/service/requestMethods.js
+++ Frontend/src/service/requestMethods.js
@@ -2,10 +2,46 @@ import axios from "axios";
 import {URL_SIMULATION_BASIC_DATA} from "../data/constants_url.js";
 
 
+export async function getAllSimulationsBasicData() {
+    try {
+        const response = await axios.get(URL_SIMULATION_BASIC_DATA);
+        return response.data;
+    } catch (error) {
+        console.log("Error in getAllSimulationsBasicData: ", error);
+    }
+}
+
+export async function getSimulationBasicDataById(id) {
+    try {
+        const response = await axios.get(URL_SIMULATION_BASIC_DATA + `${id}`);
+        return response.data;
+    } catch (error) {
+        console.log("Error in getSimulationBasicDataById: ", error);
+    }
+}
+
 export async function postSimulationBasicData(basicData) {
     try {
         await axios.post(URL_SIMULATION_BASIC_DATA, basicData);
     } catch (error) {
-        console.log("Error postSimulationParameters: ", error);
+        console.log("Error in postSimulationParameters: ", error);
     }
 }
+
+export async function updateSimulationBasicData(basicData) {
+    try {
+        await axios.put(URL_SIMULATION_BASIC_DATA, basicData);
+    } catch (error) {
+        console.log("Error in updateSimulationBasicData: ", error);
+    }
+}
+
+export async function deleteSimulationBasicDataById(id) {
+    try {
+        await axios.delete(URL_SIMULATION_BASIC_DATA + `${id}`);
+    } catch (error) {
+        console.log("Error in deleteSimulationBasicDataById: ", error);
+    }
+}
+
+
