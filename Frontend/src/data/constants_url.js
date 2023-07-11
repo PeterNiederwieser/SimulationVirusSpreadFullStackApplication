e7b1105363c3ee6dd0c03d6ead4a826bbcbd6fd4
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-11 08:34:08 +0200

    Define url constants for requests

---
+++ Frontend/src/data/constants_url.js
@@ -0,0 +1,5 @@
+export const URL_DOMAIN = `http://localhost`;
+export const URL_PORT = 8080;
+export const URL_BACKEND_BASE = `${URL_DOMAIN}:${URL_PORT}`;
+export const URL_SIMULATION_PARAMETERS = `${URL_BACKEND_BASE}/simulation-parameters`;
+export const URL_SIMULATION = `${URL_BACKEND_BASE}/simulation`;
