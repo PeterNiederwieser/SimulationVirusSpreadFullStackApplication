7b2ab5ce0ec7007b42bb48d8d4e3ff8727458045
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-12 20:50:05 +0200

    Add parameter context to behave method in interface Behaviour

--- Backend/src/main/java/com/example/Backend/simulation/logic/behaviour/Behaviour.java
+++ Backend/src/main/java/com/example/Backend/simulation/logic/behaviour/Behaviour.java
@@ -1,9 +1,10 @@
 package com.example.Backend.simulation.logic.behaviour;
 
 import com.example.Backend.simulation.data.Animal;
+import com.example.Backend.simulation.data.Context;
 
 public interface Behaviour {
-    void behave(Animal animal);
+    void behave(Animal animal, Context context);
 
     boolean matches(Animal animal);
 }
--- Backend/src/main/java/com/example/Backend/simulation/logic/behaviour/Rest.java
+++ Backend/src/main/java/com/example/Backend/simulation/logic/behaviour/Rest.java
@@ -2,10 +2,13 @@ package com.example.Backend.simulation.logic.behaviour;
 
 import com.example.Backend.simulation.data.Animal;
 import com.example.Backend.simulation.data.BehaviourType;
+import com.example.Backend.simulation.data.Context;
+import org.springframework.stereotype.Service;
 
+@Service
 public class Rest implements Behaviour {
     @Override
-    public void behave(Animal animal) {
+    public void behave(Animal animal, Context context) {
     }
 
     @Override
