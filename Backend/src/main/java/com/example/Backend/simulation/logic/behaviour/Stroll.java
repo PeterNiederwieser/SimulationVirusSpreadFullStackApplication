582cfbbcb86c66151fad0134f06a2de7f2af38cd
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-12 20:50:57 +0200

    Add behaviour class Stroll

---
+++ Backend/src/main/java/com/example/Backend/simulation/logic/behaviour/Stroll.java
@@ -0,0 +1,71 @@
+package com.example.Backend.simulation.logic.behaviour;
+
+import com.example.Backend.simulation.data.Animal;
+import com.example.Backend.simulation.data.BehaviourType;
+import com.example.Backend.simulation.data.Context;
+import com.example.Backend.simulation.data.Position;
+import com.example.Backend.simulation.logic.utils.TerritoryFieldUtils;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Stroll implements Behaviour {
+    private final TerritoryFieldUtils territoryFieldUtils;
+
+    public Stroll(TerritoryFieldUtils territoryFieldUtils) {
+        this.territoryFieldUtils = territoryFieldUtils;
+    }
+
+    @Override
+    public void behave(Animal animal, Context context) {
+        ensureCorrectVelocitySettings(animal, context);
+        performNextStep(animal, context);
+    }
+
+    private void performNextStep(Animal animal, Context context) {
+        int MAX_TRIALS_OF_DIRECTION_CHANGE = context.getMAX_TRIALS_OF_DIRECTION_CHANGE_FOR_SINGLE_MOVE();
+        int numberOfTrials = 0;
+        int nextX, nextY;
+        do {
+            nextX = Math.round((float) animal.getX() + animal.getVelocityX());
+            nextY = Math.round((float) animal.getY() + animal.getVelocityY());
+            if (isMoveInCurrentDirectionPossible(animal, new Position(nextX, nextY), context)) {
+                animal.setX(nextX);
+                animal.setY(nextY);
+                break;
+            } else {
+                setNewRandomVelocity(animal, context);
+            }
+            numberOfTrials++;
+        } while (numberOfTrials < MAX_TRIALS_OF_DIRECTION_CHANGE);
+    }
+
+    private void setNewRandomVelocity(Animal animal, Context context) {
+        float maxAnimalSpeed = animal.getMax_speed();
+        float speed = (float) Math.random() * (maxAnimalSpeed - context.getMIN_ANIMAL_SPEED()) + context.getMIN_ANIMAL_SPEED();
+        float nextVelocityX = (float) (Math.random() * speed * 2) - speed;
+        int randomSign = Math.random() < 0.5 ? 1 : -1;
+        float nextVelocityY = (float) Math.sqrt(Math.pow(speed, 2) - Math.pow(nextVelocityX, 2)) * randomSign;
+        animal.setVelocityX(nextVelocityX);
+        animal.setVelocityY(nextVelocityY);
+    }
+
+    private boolean isMoveInCurrentDirectionPossible(Animal animal, Position nextPosition, Context context) {
+        if (territoryFieldUtils.isFieldOutOfMap(nextPosition, context) || territoryFieldUtils.isFieldOccupied(animal, nextPosition, context)) {
+            return false;
+        }
+        return !territoryFieldUtils.isAreaInaccessible(nextPosition, context);
+    }
+
+    private void ensureCorrectVelocitySettings(Animal animal, Context context) {
+        float velocityX = animal.getVelocityX();
+        float velocityY = animal.getVelocityY();
+        if (velocityX == 0.0 && velocityY == 0.0) {
+            setNewRandomVelocity(animal, context);
+        }
+    }
+
+    @Override
+    public boolean matches(Animal animal) {
+        return animal.getBehaviourType().equals(BehaviourType.STROLL);
+    }
+}
