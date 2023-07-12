645411d5f502ca6e36690fe7c4a387bf775dda8d
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-12 19:00:19 +0200

    Add RestController SimulationDataEndpoint

---
+++ Backend/src/main/java/com/example/Backend/api/SimulationDataEndpoint.java
@@ -0,0 +1,25 @@
+package com.example.Backend.api;
+
+import com.example.Backend.persistence.entity.SimulationData;
+import com.example.Backend.service.SimulationDataService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("simulation-basic-parameters")
+@CrossOrigin(origins = "*")
+public class SimulationDataEndpoint {
+    private final SimulationDataService simulationDataService;
+
+    public SimulationDataEndpoint(SimulationDataService simulationDataService) {
+        this.simulationDataService = simulationDataService;
+    }
+    @GetMapping
+    public List<SimulationData> get() {
+        return simulationDataService.findAll();
+    }
+}
