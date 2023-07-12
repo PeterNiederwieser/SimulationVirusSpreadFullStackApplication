ac8bf09656104abae665c08b1b51955103bc31fe
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-12 18:45:21 +0200

    Add new methods findBySimulationNameAllIgnoreCase and findByIsSimulationCompletedEquals in SimulationBasicParametersRepository

--- Backend/src/main/java/com/example/Backend/persistence/repository/SimulationBasicDataRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Backend.persistence.repository;
-
-import com.example.Backend.persistence.entity.SimulationBasicData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SimulationBasicDataRepository extends JpaRepository<SimulationBasicData, Long> {
-}
---
+++ Backend/src/main/java/com/example/Backend/persistence/repository/SimulationBasicParametersRepository.java
@@ -0,0 +1,12 @@
+package com.example.Backend.persistence.repository;
+
+import com.example.Backend.persistence.entity.SimulationBasicParameters;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SimulationBasicParametersRepository extends JpaRepository<SimulationBasicParameters, Long> {
+    List<SimulationBasicParameters> findBySimulationNameAllIgnoreCase(String simulationName);
+
+    List<SimulationBasicParameters> findByIsSimulationCompletedEquals(boolean value);
+}
