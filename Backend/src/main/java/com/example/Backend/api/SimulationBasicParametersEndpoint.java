307fdd7d60cd2246cdd38ff0e4dc52f17d05ec88
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-12 18:44:47 +0200

    Add new methods getByName and getByStatusOfSimulationCompletion

---
+++ Backend/src/main/java/com/example/Backend/api/SimulationBasicParametersEndpoint.java
@@ -0,0 +1,54 @@
+package com.example.Backend.api;
+
+import com.example.Backend.persistence.entity.SimulationBasicParameters;
+import com.example.Backend.service.SimulationBasicParametersService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("simulation-basic-parameters")
+@CrossOrigin(origins = "*")
+public class SimulationBasicParametersEndpoint {
+    private final SimulationBasicParametersService simulationBasicParametersService;
+
+    public SimulationBasicParametersEndpoint(SimulationBasicParametersService simulationBasicParametersService) {
+        this.simulationBasicParametersService = simulationBasicParametersService;
+    }
+
+    @GetMapping
+    public List<SimulationBasicParameters> get() {
+        return simulationBasicParametersService.findAll();
+    }
+
+    @GetMapping("{id}")
+    public SimulationBasicParameters getOne(@PathVariable long id) throws ElementNotFoundException {
+        return simulationBasicParametersService.findById(id)
+                .orElseThrow(ElementNotFoundException::new);
+    }
+
+    @PostMapping
+    SimulationBasicParameters save(@RequestBody SimulationBasicParameters simulationBasicParameters) {
+        return simulationBasicParametersService.save(simulationBasicParameters);
+    }
+
+    @PutMapping
+    SimulationBasicParameters update(@RequestBody SimulationBasicParameters simulationBasicParameters) {
+        return simulationBasicParametersService.save(simulationBasicParameters);
+    }
+
+    @DeleteMapping("{id}")
+    void deleteOne(@PathVariable long id) {
+        simulationBasicParametersService.deleteById(id);
+    }
+
+    @GetMapping("getByName/{name}")
+    List<SimulationBasicParameters> getByName(@PathVariable String name) {
+        return simulationBasicParametersService.findByName(name);
+    }
+
+    @GetMapping("isSimulationCompleted/{value}")
+    List<SimulationBasicParameters> getByStatusOfSimulationCompletion(@PathVariable boolean value) {
+        return simulationBasicParametersService.findByStatusOfSimulationCompletion(value);
+    }
+}
