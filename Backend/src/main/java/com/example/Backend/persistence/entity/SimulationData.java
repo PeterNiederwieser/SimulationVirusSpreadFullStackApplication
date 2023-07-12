2ff1308991e57fbf390dadf72bf277e0dc7ffdab
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-12 18:59:55 +0200

    Add entity SimulationData

---
+++ Backend/src/main/java/com/example/Backend/persistence/entity/SimulationData.java
@@ -0,0 +1,24 @@
+package com.example.Backend.persistence.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SimulationData {
+    @Id
+    @GeneratedValue
+    private long id;
+    private int stepNumber;
+    private double xPosition;
+    private double yPosition;
+    private String healthState;
+}
