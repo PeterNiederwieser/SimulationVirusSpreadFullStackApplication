e965c7bd8d3f0e38789ba1d1d34c4437abd9dc6f
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-13 16:12:27 +0200

    Refactor TerritoryCreator

--- Backend/src/main/java/com/example/Backend/simulation/logic/territory/TerritoryCreator.java
+++ Backend/src/main/java/com/example/Backend/simulation/logic/territory/TerritoryCreator.java
@@ -13,18 +13,18 @@ import java.io.IOException;
 
 @Service
 public class TerritoryCreator {
-    public void generateMapFromImage(Context context) throws IOException {
-        String filePathOfImage = context.getFilePathOfMapImage();
+    public void generateTerritoryFromImage(Context context) throws IOException {
+        String filePathOfImage = context.getFilePathOfTerritoryImage();
         BufferedImage image = ImageIO.read(new File(filePathOfImage));
-        SurfaceType[][] map = new SurfaceType[context.getMAP_HEIGHT()][context.getMAP_WIDTH()];
-        for (int x = 0; x < context.getMAP_HEIGHT(); x++) {
-            for (int y = 0; y < context.getMAP_WIDTH(); y++) {
+        SurfaceType[][] map = new SurfaceType[context.getTERRITORY_HEIGHT()][context.getTERRITORY_WIDTH()];
+        for (int x = 0; x < context.getTERRITORY_HEIGHT(); x++) {
+            for (int y = 0; y < context.getTERRITORY_WIDTH(); y++) {
                 Color pixelColor = new Color(image.getRGB(x, y));
                 SurfaceType surfaceType = getSurfaceTypeFromColor(pixelColor);
                 map[x][y] = surfaceType;
             }
         }
-        context.setMap(map);
+        context.setTerritory(map);
     }
 
     private SurfaceType getSurfaceTypeFromColor(Color pixelColor) {
