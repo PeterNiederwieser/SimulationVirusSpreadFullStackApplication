676e1615a8ce4de4b1e84abc0a079b2f61b7dae0
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-13 15:50:37 +0200

    Add constructor to class Animal

--- Backend/src/main/java/com/example/Backend/simulation/data/Animal.java
+++ Backend/src/main/java/com/example/Backend/simulation/data/Animal.java
@@ -1,8 +1,12 @@
 package com.example.Backend.simulation.data;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Animal {
     private int x;
     private int y;
@@ -15,4 +19,13 @@ public class Animal {
     private int startOfSevereIllness;
     private int timeOfPossibleDeathAfterInfection;
     private boolean isGettingSeverelyIll;
+    public Animal(int x, int y, float max_speed, HealthState healthState, BehaviourType behaviourType, int timeOfPossibleDeathAfterInfection, boolean isDyingInCaseOfInfection) {
+        this.x = x;
+        this.y = y;
+        this.max_speed = max_speed;
+        this.healthState = healthState;
+        this.behaviourType = behaviourType;
+        this.timeOfPossibleDeathAfterInfection = timeOfPossibleDeathAfterInfection;
+        this.isGettingSeverelyIll = isDyingInCaseOfInfection;
+    }
 }
