25803df1d4c14f08f80f64a51f5bfd1f54ecd616
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-13 16:10:13 +0200

    Add interface Phase

---
+++ Backend/src/main/java/com/example/Backend/simulation/logic/simulationPhase/Phase.java
@@ -0,0 +1,7 @@
+package com.example.Backend.simulation.logic.simulationPhase;
+
+import com.example.Backend.simulation.data.Context;
+
+public interface Phase {
+    void perform(Context context);
+}
