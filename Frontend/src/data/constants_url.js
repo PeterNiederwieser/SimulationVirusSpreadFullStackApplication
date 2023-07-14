99c7812c5eff8a710ac27acdc1f471f277984c54
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-14 18:49:23 +0200

    Add URL_SIMULATION_DATA to constants_url.js

--- Frontend/src/data/constants_url.js
+++ Frontend/src/data/constants_url.js
@@ -2,3 +2,4 @@ export const URL_DOMAIN = `http://localhost`;
 export const URL_PORT = 8080;
 export const URL_BACKEND_BASE = `${URL_DOMAIN}:${URL_PORT}`;
 export const URL_SIMULATION_BASIC_DATA = `${URL_BACKEND_BASE}/simulation-basic-parameters`;
+export const URL_SIMULATION_DATA = `${URL_BACKEND_BASE}/simulation-data`;
