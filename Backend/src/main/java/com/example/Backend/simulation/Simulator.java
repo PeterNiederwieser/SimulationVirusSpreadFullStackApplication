62840966f418d79257b5caceac50f2700a62acfd
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-14 14:28:57 +0200

    Add Simulator

---
+++ Backend/src/main/java/com/example/Backend/simulation/Simulator.java
@@ -0,0 +1,27 @@
+package com.example.Backend.simulation;
+
+import com.example.Backend.simulation.data.Context;
+import com.example.Backend.simulation.logic.simulationPhase.Phase;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Simulator {
+    private final List<Phase> phases;
+
+    public Simulator(List<Phase> phases) {
+        this.phases = phases;
+    }
+
+    public void simulateNextSteps(int numberOfSteps, Context context) throws IOException {
+        for (int i = 0; i < numberOfSteps; i++) {
+            simulatePhases(context);
+        }
+    }
+
+    private void simulatePhases(Context context) {
+        for (Phase phase : phases) {
+            phase.perform(context);
+        }
+    }
+}
