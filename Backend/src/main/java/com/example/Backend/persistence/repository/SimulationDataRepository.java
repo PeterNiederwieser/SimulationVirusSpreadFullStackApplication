0c61ab205cb7f9d421fc0d5580c6e6ca68caa80b
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-14 14:28:00 +0200

    Add findTopNByOrderByStepNumberDesc method to SimulationDataRepository

--- Backend/src/main/java/com/example/Backend/persistence/repository/SimulationDataRepository.java
+++ Backend/src/main/java/com/example/Backend/persistence/repository/SimulationDataRepository.java
@@ -3,5 +3,8 @@ package com.example.Backend.persistence.repository;
 import com.example.Backend.persistence.entity.SimulationData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SimulationDataRepository extends JpaRepository<SimulationData, Long> {
+    List<SimulationData> findTopNByOrderByStepNumberDesc(int numberOfSteps);
 }
