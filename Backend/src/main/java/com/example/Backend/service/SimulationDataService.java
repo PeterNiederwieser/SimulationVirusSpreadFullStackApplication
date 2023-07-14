262dc439a79f75054cc993d5766c107bfac88558
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-14 23:10:40 +0200

    Adapt methods in SimulationDataService get data in given range

--- Backend/src/main/java/com/example/Backend/service/SimulationDataService.java
+++ Backend/src/main/java/com/example/Backend/service/SimulationDataService.java
@@ -2,22 +2,24 @@ package com.example.Backend.service;
 
 import com.example.Backend.persistence.entity.SimulationData;
 import com.example.Backend.persistence.repository.SimulationDataRepository;
+import com.example.Backend.simulation.SimulationManager;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
 public class SimulationDataService {
     private final SimulationDataRepository simulationDataRepository;
+    private final SimulationManager simulationManager;
 
-    public SimulationDataService(SimulationDataRepository simulationDataRepository) {
+    public SimulationDataService(SimulationDataRepository simulationDataRepository, SimulationManager simulationManager) {
         this.simulationDataRepository = simulationDataRepository;
+        this.simulationManager = simulationManager;
     }
 
-    public List<SimulationData> findNextSimulationSteps(int numberOfSteps) {
-        return simulationDataRepository.findTopNByOrderByStepNumberDesc(numberOfSteps);
-    }
-    public SimulationData save(SimulationData simulationData) {
-        return simulationDataRepository.save(simulationData);
+    public List<SimulationData> getSimulationData(long simulationId, int stepNumberLowerBorder, int stepNumberUpperBorder) throws IOException {
+        simulationManager.runDemandedSimulationSteps(simulationId,500);
+        return simulationDataRepository.findBySimulationIdAndStepNumberBetween(simulationId, stepNumberLowerBorder, stepNumberUpperBorder);
     }
 }
