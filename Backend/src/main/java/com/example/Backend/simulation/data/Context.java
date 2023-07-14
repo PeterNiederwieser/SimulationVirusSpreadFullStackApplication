0114fad1021a0ef8d722def138ce8c1a50e27dc6
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-14 15:55:18 +0200

    Add field simulationId in Context

--- Backend/src/main/java/com/example/Backend/simulation/data/Context.java
+++ Backend/src/main/java/com/example/Backend/simulation/data/Context.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 @Data
 public class Context {
+    private long simulationId;
     private int NUMBER_OF_ANIMALS;
     private int NUMBER_OF_INITIAL_INFECTIONS;
     private float PROBABILITY_OF_INFECTION;
@@ -14,7 +15,8 @@ public class Context {
     private List<Animal> population = new ArrayList<>();
     private SurfaceType[][] territory;
 
-    public Context(int NUMBER_OF_ANIMALS, int NUMBER_OF_INITIAL_INFECTIONS, float PROBABILITY_OF_INFECTION, float PROBABILITY_OF_FATAL_INFECTION_COURSE) {
+    public Context(long simulationId, int NUMBER_OF_ANIMALS, int NUMBER_OF_INITIAL_INFECTIONS, float PROBABILITY_OF_INFECTION, float PROBABILITY_OF_FATAL_INFECTION_COURSE) {
+        this.simulationId = simulationId;
         this.NUMBER_OF_ANIMALS = NUMBER_OF_ANIMALS;
         this.NUMBER_OF_INITIAL_INFECTIONS = NUMBER_OF_INITIAL_INFECTIONS;
         this.PROBABILITY_OF_INFECTION = PROBABILITY_OF_INFECTION;
