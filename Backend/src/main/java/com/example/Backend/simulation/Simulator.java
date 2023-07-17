4413aa56c681f36102b7144c5c0e2e8f1ea45a82
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-17 20:04:28 +0200

    Refactor Simulator

--- Backend/src/main/java/com/example/Backend/simulation/Simulator.java
+++ Backend/src/main/java/com/example/Backend/simulation/Simulator.java
@@ -4,23 +4,17 @@ import com.example.Backend.simulation.data.Context;
 import com.example.Backend.simulation.logic.simulationPhase.Phase;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.util.List;
 @Service
 public class Simulator {
-    private final List<Phase> phases;
 
-    public Simulator(List<Phase> phases) {
-        this.phases = phases;
-    }
-
-    public void simulateNextSteps(int numberOfSteps, Context context) throws IOException {
-        for (int i = 0; i < numberOfSteps; i++) {
-            simulatePhases(context);
+    public void simulate(Context context, List<Phase> phases) {
+        while(context.getNumberOfInfections() != 0) {
+            simulatePhases(context, phases);
         }
     }
 
-    private void simulatePhases(Context context) {
+    private void simulatePhases(Context context, List<Phase> phases) {
         for (Phase phase : phases) {
             phase.perform(context);
         }
