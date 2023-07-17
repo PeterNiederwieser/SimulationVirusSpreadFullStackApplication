631974486629a2e2607f438223805e8044e91843
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-17 20:00:43 +0200

    Adapt SimulationDataWebSocketEndpoint to retrieving data either from Db or SimulationDataStorage

--- Backend/src/main/java/com/example/Backend/api/SimulationDataWebSocketEndpoint.java
+++ Backend/src/main/java/com/example/Backend/api/SimulationDataWebSocketEndpoint.java
@@ -3,25 +3,25 @@ package com.example.Backend.api;
 import com.example.Backend.api.webSocketConfiguration.data.RequestBodySimData;
 import com.example.Backend.persistence.entity.SimulationData;
 import com.example.Backend.service.SimulationDataService;
+import com.example.Backend.simulation.SimulationManager;
+import com.example.Backend.simulation.data.Context;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.websocket.Session;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.socket.WebSocketMessage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class SimulationDataWebSocketEndpoint {
     private final SimulationDataService simulationDataService;
+    private final SimulationManager simulationManager;
 
-    public SimulationDataWebSocketEndpoint(SimulationDataService simulationDataService) {
+    public SimulationDataWebSocketEndpoint(SimulationDataService simulationDataService, SimulationManager simulationManager) {
         this.simulationDataService = simulationDataService;
+        this.simulationManager = simulationManager;
     }
 
     @MessageMapping("/request")
@@ -31,12 +31,28 @@ public class SimulationDataWebSocketEndpoint {
         System.out.println("stepNumberCeil = " + request.stepNumberCeil());
         System.out.println("stepNumberFloor = " + request.stepNumberFloor());
         System.out.println("simulationId = " + request.simulationId());
-        List<SimulationData> simulationData = simulationDataService.getSimulationData(request.simulationId(), request.stepNumberFloor(), request.stepNumberCeil());
+        long simulationId = request.simulationId();
+        Context context = simulationManager.getSimulationContext(simulationId);
+        List<SimulationData> requestedSimulationData;
+        startSimulationIfNotStartedYet(simulationId, context);
+        requestedSimulationData = getRequestedSimulationData(request, context);
+        System.out.println("requestedSimulationData.size() = " + requestedSimulationData.size());
         ObjectMapper objectMapper = new ObjectMapper();
-        String jsonData = objectMapper.writeValueAsString(simulationData);
-        // System.out.println("jsonData: " + jsonData);
-        //session.getBasicRemote().sendText(jsonData);
-        System.out.println("jsonData: " + jsonData);
-        return jsonData;
+        return objectMapper.writeValueAsString(requestedSimulationData);
+    }
+
+    private void startSimulationIfNotStartedYet(long simulationId, Context context) throws IOException {
+        if (!context.isSimulationsStartBeenTriggered()) {
+            simulationManager.runSimulation(simulationId, context);
+            context.setSimulationsStartBeenTriggered(true);
+        }
+    }
+
+    private List<SimulationData> getRequestedSimulationData(RequestBodySimData request, Context context) throws IOException {
+        return context.isCompleteSimulationDataSavedToDb() ?
+                simulationDataService.getSimulationData(request.simulationId(), request.stepNumberFloor(), request.stepNumberCeil()) :
+                context.getSimulationDataStorage().stream()
+                        .filter(simulationData -> simulationData.getStepNumber() >= request.stepNumberFloor() && simulationData.getStepNumber() < request.stepNumberCeil())
+                        .toList();
     }
 }
