2d24c49701654c1435f990564b07e0d8ee363176
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-17 14:25:05 +0200

    Refactor OverviewSimulations.jsx

--- Frontend/src/ui/components/OverviewSimulations.jsx
+++ Frontend/src/ui/components/OverviewSimulations.jsx
@@ -2,11 +2,13 @@ import OverviewItem from "./OverviewItem.jsx";
 
 function OverviewSimulations({runSimulation, simulationsBasicData, setSimulationsBasicData}) {
     if(simulationsBasicData == null) {
-        return;
+        return (
+            <div className="overview-simulations">Overview of your simulations:</div>
+        );
     }
     return (
         <div className="overview-simulations">
-            OverviewSimulations:
+            Overview of your simulations:
             {simulationsBasicData.map(item => (
                 <OverviewItem key={item.id} item={item} runSimulation={runSimulation} setSimulationsBasicData={setSimulationsBasicData}/>
             ))
