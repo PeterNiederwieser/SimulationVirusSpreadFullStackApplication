eeabbe377665c1808a06139bbd3d5bdc8100ef3d
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-17 15:35:21 +0200

    Add SimulationSection for canvas and diagrams

---
+++ Frontend/src/ui/components/SimulationSection.jsx
@@ -0,0 +1,13 @@
+import Canvas from "./Canvas.jsx";
+import Diagrams from "./Diagrams.jsx";
+
+function SimulationSection() {
+    return (
+        <div className="simulation-section">
+            <Canvas/>
+            <Diagrams/>
+        </div>
+    )
+}
+
+export default SimulationSection
