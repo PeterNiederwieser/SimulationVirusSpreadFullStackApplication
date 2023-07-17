26ebf82b33cdd2a10839652599c744a27ecd05a0
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-17 20:04:14 +0200

    Refactor SimulationManager

--- Backend/src/main/java/com/example/Backend/simulation/SimulationManager.java
+++ Backend/src/main/java/com/example/Backend/simulation/SimulationManager.java
@@ -1,24 +1,45 @@
 package com.example.Backend.simulation;
 
+import com.example.Backend.persistence.repository.SimulationDataRepository;
 import com.example.Backend.service.SimulationContextStorage;
 import com.example.Backend.simulation.data.Context;
-import com.example.Backend.simulation.logic.initialisation.Initializer;
+import com.example.Backend.simulation.logic.simulationPhase.Phase;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
+import java.util.List;
+
 @Service
 public class SimulationManager {
-    private final Initializer initializer;
     private final SimulationContextStorage simulationContextStorage;
     private final Simulator simulator;
+    private final List<Phase> phases;
+    private final SimulationDataRepository simulationDataRepository;
 
-    public SimulationManager(Initializer initializer, SimulationContextStorage simulationContextStorage, Simulator simulator) {
-        this.initializer = initializer;
+    public SimulationManager(SimulationContextStorage simulationContextStorage, Simulator simulator, List<Phase> phases, SimulationDataRepository simulationDataRepository) {
         this.simulationContextStorage = simulationContextStorage;
         this.simulator = simulator;
+        this.phases = phases;
+        this.simulationDataRepository = simulationDataRepository;
     }
-    public void runDemandedSimulationSteps(long simulationId, int numberOfSteps) throws IOException {
-        Context context = simulationContextStorage.getContext(simulationId);
-        simulator.simulateNextSteps(numberOfSteps, context);
+
+    public void runSimulation(Context context) {
+        simulator.simulate(context, phases);
     }
+
+    public Context getSimulationContext(long simulationId) {
+        return simulationContextStorage.getContext(simulationId);
+    }
+
+    /*public void saveSimulationToDbAsynchronously(long id) {
+        Context context = simulationContextStorage.getContext(id);
+        List<SimulationData> simulationDataStorage = context.getSimulationDataStorage();
+        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+            try {
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> future.cancel(true)));
+    }*/
 }
