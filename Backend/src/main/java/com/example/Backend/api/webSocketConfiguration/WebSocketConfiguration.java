32f206c4bd2f756adc487de666c41c8bff96f9ae
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-17 12:54:21 +0200

    Refactor WebSocketConfiguration

--- Backend/src/main/java/com/example/Backend/api/webSocketConfiguration/WebSocketConfiguration.java
+++ Backend/src/main/java/com/example/Backend/api/webSocketConfiguration/WebSocketConfiguration.java
@@ -2,9 +2,12 @@ package com.example.Backend.api.webSocketConfiguration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@CrossOrigin
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
@@ -13,8 +16,9 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
         configuration.enableSimpleBroker("/topic");
         configuration.setApplicationDestinationPrefixes("/app");
     }
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("websocket-endpoint").setAllowedOriginPatterns("*");
+        registry.addEndpoint("/websocket-endpoint").setAllowedOrigins("*");
     }
 }
