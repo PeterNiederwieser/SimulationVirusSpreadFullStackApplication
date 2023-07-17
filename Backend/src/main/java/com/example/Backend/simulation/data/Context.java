a34827122cbf400429d39317ebde1351c986ad3c
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-17 19:53:52 +0200

    Add new fields to class Context

--- Backend/src/main/java/com/example/Backend/simulation/data/Context.java
+++ Backend/src/main/java/com/example/Backend/simulation/data/Context.java
@@ -1,5 +1,6 @@
 package com.example.Backend.simulation.data;
 
+import com.example.Backend.persistence.entity.SimulationData;
 import lombok.Data;
 
 import java.util.ArrayList;
@@ -15,10 +16,15 @@ public class Context {
     private int stepNumber = 1;
     private List<Animal> population = new ArrayList<>();
     private SurfaceType[][] territory;
+    private int numberOfInfections;
+    private List<SimulationData> simulationDataStorage = new ArrayList<>();
+    private boolean isCompleteSimulationDataSavedToDb = false;
+    private boolean isSimulationsStartBeenTriggered = false;
 
     public Context(long simulationId, int NUMBER_OF_ANIMALS, int NUMBER_OF_INITIAL_INFECTIONS) {
         this.simulationId = simulationId;
         this.NUMBER_OF_ANIMALS = NUMBER_OF_ANIMALS;
         this.NUMBER_OF_INITIAL_INFECTIONS = NUMBER_OF_INITIAL_INFECTIONS;
+        this.numberOfInfections = NUMBER_OF_INITIAL_INFECTIONS;
     }
 }
