2f9cc0f0ab47d56585123810fba80978c8802276
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-18 09:31:36 +0200

    Adjust WebSocketFunctions to useState isGraphicsShown

--- Frontend/src/service/WebSocketFunctions.js
+++ Frontend/src/service/WebSocketFunctions.js
@@ -1,21 +1,21 @@
 import {NUMBER_OF_SIM_DATA_PER_REQUEST} from "../data/constants.js";
 import * as StompJs from "@stomp/stompjs";
 
-export function setupWebSocket(receivedSimulationData) {
+export function setupWebSocket(receivedSimulationData, setIsGraphicsShown) {
     const stompClient = new StompJs.Client({
         brokerURL: "ws://localhost:8080/websocket-endpoint"
     });
 
     stompClient.onConnect = () => {
-        console.log("WebSocket Connection established Simulation: ");
+        console.log("WebSocket Connection established: ");
         stompClient.subscribe("/topic/data", (data) => {
-            console.log("response: ", JSON.parse(data.body));
             const dataArray = JSON.parse(data.body);
             dataArray.forEach(item => {
                 receivedSimulationData.current = [...receivedSimulationData.current, item];
             })
-            console.log("receivedSimulationData: ", receivedSimulationData.current);
-            console.log("number of elements in receivedSimulationData: ", receivedSimulationData.length);
+            if (receivedSimulationData.current.length > 0) {
+                setIsGraphicsShown(true);
+            }
         });
     };
 
@@ -27,7 +27,7 @@ export function setupWebSocket(receivedSimulationData) {
     return stompClient;
 }
 
-export function getSimulationData(simulationId, receivedSimulationData, stompClient) {
+export function getSimulationData(simulationId, stompClient) {
     let stepNumberFloor = 0;
     let stepNumberCeil = NUMBER_OF_SIM_DATA_PER_REQUEST;
 
@@ -43,5 +43,5 @@ export function getSimulationData(simulationId, receivedSimulationData, stompCli
         })
         stepNumberFloor += NUMBER_OF_SIM_DATA_PER_REQUEST;
         stepNumberCeil += NUMBER_OF_SIM_DATA_PER_REQUEST;
-    }, 4000);
+    }, 700);
 }
