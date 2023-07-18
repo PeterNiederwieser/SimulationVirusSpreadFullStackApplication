6053c45fa3e9c7e8091c135a0cd64c7dc0008e8d
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-18 19:54:15 +0200

    Add constants for WebSocket to constants_url.js

--- Frontend/src/data/constants_url.js
+++ Frontend/src/data/constants_url.js
@@ -3,3 +3,6 @@ export const URL_PORT = 8080;
 export const URL_BACKEND_BASE = `${URL_DOMAIN}:${URL_PORT}`;
 export const URL_SIMULATION_BASIC_DATA = `${URL_BACKEND_BASE}/simulation-basic-parameters`;
 export const URL_SIMULATION_DATA = `${URL_BACKEND_BASE}/simulation-data`;
+export const URL_WEBSOCKET_ENDPOINT = "ws://localhost:8080/websocket-endpoint";
+export const WEBSOCKET_SUBSCRIPTION_DESTINATION = "/topic/data";
+export const WEBSOCKET_PUBLISH_DESTINATION = "/app/request";
