e11d321e4f8eb481b46997e5fb41fe27c09ac74b
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-18 19:54:38 +0200

    Remove PROBABILITY_OF_INFECTION and PROBABILITY_OF_FATAL_INFECTION_COURSE to MainConstants

--- Backend/src/main/java/com/example/Backend/simulation/data/Context.java
+++ Backend/src/main/java/com/example/Backend/simulation/data/Context.java
@@ -11,8 +11,6 @@ public class Context {
     private long simulationId;
     private int NUMBER_OF_ANIMALS;
     private int NUMBER_OF_INITIAL_INFECTIONS;
-    private float PROBABILITY_OF_INFECTION = 0.8F;
-    private float PROBABILITY_OF_FATAL_INFECTION_COURSE = 0.5F;
     private int stepNumber = 1;
     private List<Animal> population = new ArrayList<>();
     private SurfaceType[][] territory;
