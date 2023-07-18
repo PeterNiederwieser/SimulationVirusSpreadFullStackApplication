0b30288c5472282520cf383c75a9c19c96c5f688
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-18 21:58:07 +0200

    Add props in SimulationSection.jsx

--- Frontend/src/ui/components/SimulationSection.jsx
+++ Frontend/src/ui/components/SimulationSection.jsx
@@ -1,15 +1,19 @@
 import Canvas from "./Canvas.jsx";
 import Diagrams from "./Diagrams.jsx";
 
-function SimulationSection({receivedSimulationData, isGraphicsShown}) {
+function SimulationSection({receivedSimulationData, isSimulationRunning,  setIsSimulationRunning, stompClient, selectedSimulationId}) {
     return (
         <div className="simulation-section">
             <Canvas
                 receivedSimulationData={receivedSimulationData}
-                isGraphicsShown={isGraphicsShown}
+                isSimulationRunning={isSimulationRunning}
+                setIsSimulationRunning={setIsSimulationRunning}
+                stompClient={stompClient}
+                selectedSimulationId={selectedSimulationId}
             />
             <Diagrams
                 receivedSimulationData={receivedSimulationData}
+                isSimulationRunning={isSimulationRunning}
             />
         </div>
     )
