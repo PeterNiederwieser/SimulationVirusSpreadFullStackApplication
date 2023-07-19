7c439ecf225f174bbb2996b730e2c5b0121627dd
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-19 21:47:14 +0200

    Add statistics to Canvas.jsx

--- Frontend/src/ui/components/Canvas.jsx
+++ Frontend/src/ui/components/Canvas.jsx
@@ -1,12 +1,16 @@
 import {useEffect, useRef, useState} from "react";
 import {getSimulationData} from "../../service/webSocketFunctions.js";
 import {
-    HEIGHT_CANVAS, LIMIT_DATA_AMOUNT_FOR_NEW_REQUEST, NUMBER_OF_SIM_STEPS_PER_REQUEST,
+    HEIGHT_CANVAS,
+    LIMIT_DATA_AMOUNT_FOR_NEW_REQUEST,
+    NUMBER_OF_SIM_STEPS_PER_REQUEST,
+    TIME_RANGE_FOR_STATISTICS_IN_STEPS,
     TIMEOUT_FOR_SIMULATION_REPAINT_IN_MS,
     WIDTH_CANVAS
 } from "../../data/constants.js";
-import {drawAnimals, drawBackground} from "../../service/drawing.js";
+import {drawBackground, processDataPerStepNumber} from "../../service/drawing.js";
 import {handleStopContinueButton} from "../../service/eventHandler.js";
+import {Chart} from "react-google-charts";
 
 function Canvas({
                     receivedSimulationData,
@@ -21,8 +25,22 @@ function Canvas({
     const stepNumberCeilRef = useRef(NUMBER_OF_SIM_STEPS_PER_REQUEST);
     const [buttonText, setButtonText] = useState("Stop");
     const [isSimulationPaused, setIsSimulationPaused] = useState(false);
+    const [pieChartData, setPieChartData] = useState([]);
     let intervalId = null;
     let context = null;
+    const statistics = {
+        newInfectionsPerTimeRange: [],
+        newDeathsPerTimeRange: [],
+        timeRange: TIME_RANGE_FOR_STATISTICS_IN_STEPS,
+        totalNumberOfInitialAnimals: 0,
+        totalNumberOfCurrentHealthyAnimals: 0,
+        totalNumberOfCurrentRecoveredAnimals: 0,
+        totalNumberOfCurrentInfectedAnimals: 0,
+        totalNumberOfCurrentSeverelyIllAnimals: 0,
+        totalNumberOfDeadAnimals: 0,
+        bufferNewInfections: 0,
+        bufferDeaths: 0
+    }
 
     useEffect(() => {
         clearInterval(intervalId);
@@ -48,18 +66,33 @@ function Canvas({
             stepNumberRef.current = stepNumberRef.current + 1;
             context.clearRect(0, 0, WIDTH_CANVAS, HEIGHT_CANVAS);
             drawBackground(context);
-            drawAnimals(context, receivedSimulationData, stepNumberRef.current);
+            processDataPerStepNumber(context, receivedSimulationData, stepNumberRef.current, statistics);
         }
+    }
+
+    const optionsPieChart = {
+        title: ""
     }
 
     return (
         <>
             {isSimulationRunning &&
-                (<div className="canvas">
-                    <canvas ref={canvasRef} width={WIDTH_CANVAS} height={HEIGHT_CANVAS}/>
-                    <button type="button"
-                            onClick={() => handleStopContinueButton(isSimulationPaused, setIsSimulationPaused, intervalId, setButtonText)}>{buttonText}</button>
-                </div>)}
+                (<div>
+                    <div className="canvas">
+                        <canvas ref={canvasRef} width={WIDTH_CANVAS} height={HEIGHT_CANVAS}/>
+                        <button type="button"
+                                onClick={() => handleStopContinueButton(isSimulationPaused, setIsSimulationPaused, intervalId, setButtonText)}>{buttonText}</button>
+                    </div>
+                    <Chart
+                        chartType="PieChart"
+                        data={pieChartData}
+                        options={optionsPieChart}
+                        width={"100%"}
+                        height={"400px"}
+                        legendToggle={true}
+                    />
+                </div>)
+            }
         </>
     )
 }
