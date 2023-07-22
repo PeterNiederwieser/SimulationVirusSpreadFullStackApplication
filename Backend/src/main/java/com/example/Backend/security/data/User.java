75a7fd523370a79e21937091260e9b20532b0fe5
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-22 15:19:11 +0200

    Add entity User

---
+++ Backend/src/main/java/com/example/Backend/security/data/User.java
@@ -0,0 +1,23 @@
+package com.example.Backend.security.data;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "_user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String username;
+    private String password;
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<String> authorities;
+}
