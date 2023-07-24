c1a457eb984286e95b69b9d986416f08e2c2d603
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-24 15:37:04 +0200

    Add path for Register in App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,17 @@ import './App.css';
 import {RouterProvider, createBrowserRouter} from "react-router-dom";
 import Landing from "./ui/SignIn.jsx";
 import Simulation from "./ui/Simulation.jsx";
+import Register from "./ui/Register.jsx";
 
 function App() {
     const router = createBrowserRouter([
         {
             path: "/",
             element: <Landing/>
+        },
+        {
+            path: "/register",
+            element: <Register/>
         },
         {
             path: "/simulation",
