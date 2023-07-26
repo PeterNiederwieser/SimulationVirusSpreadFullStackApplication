d9e62b45cfb7898fa9ad0c497436dcbe49a50652
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-26 13:20:41 +0200

    Transform input field to material ui textfield in FormInput.jsx

--- Frontend/src/ui/components/FormInput.jsx
+++ Frontend/src/ui/components/FormInput.jsx
@@ -1,13 +1,31 @@
-function FormInput({updateFormObject, value, name, label, classNameLabel, classNameInput}) {
+import TextField from "@mui/material/TextField";
+import * as React from "react";
+
+function FormInput({updateFormObject, value, name, label, id}) {
     return (
         <div className="form-input">
-            <div className={classNameLabel}>{label}</div>
-            <input
-                className={classNameInput}
-                type="text"
-                value={value}
+            <TextField
+                margin="normal"
+                required
+                fullWidth
                 name={name}
+                value={value}
+                label={label}
+                id={id}
                 onChange={event => updateFormObject(event.target.name, event.target.value)}
+                sx={{
+                    "& .MuiInputLabel-root": {color: '#45A29E'},
+                    "& .MuiOutlinedInput-root": {
+                        "& > fieldset": {borderColor: '#1F2833', borderRadius: 5, backgroundColor: '#1F2833'},
+                    },
+                    "& .MuiInputLabel-root.Mui-focused": {color: '#45A29E', zIndex: 3},
+                    "& .MuiOutlinedInput-root.Mui-focused": {
+                        "& > fieldset": {borderColor: '#45A29E', backgroundColor: '#1F2833'}
+                    },
+                    "& input": {
+                        color: 'white', zIndex: 1
+                    }
+                }}
             />
         </div>
     )
