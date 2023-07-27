77f23f0a2da7863f1d18ff3f81b7807732805dc1
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-27 10:49:46 +0200

    Add stylingElements.js

---
+++ Frontend/src/data/stylingElements.js
@@ -0,0 +1,13 @@
+export const stylingTextfield = {
+    "& .MuiInputLabel-root": {color: '#FFFFFF'},
+    "& .MuiOutlinedInput-root": {
+        "& > fieldset": {borderColor: '#3481cb', borderRadius: 5, backgroundColor: '#173e81'},
+    },
+    "& .MuiInputLabel-root.Mui-focused": {color: '#FFFFFF', zIndex: 3},
+    "& .MuiOutlinedInput-root.Mui-focused": {
+        "& > fieldset": {borderColor: '#FFFFFF', backgroundColor: '#173e81'}
+    },
+    "& input": {
+        color: '#FFFFFF', zIndex: 1
+    }
+};
