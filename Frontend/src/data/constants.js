a3846dcf4381f8fa2e29860f61a5ed629cb6fceb
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-07-27 10:43:26 +0200

    Add FILE_PATH_IMAGE_SIGNIN_REGISTER to constants.js

--- Frontend/src/data/constants.js
+++ Frontend/src/data/constants.js
@@ -4,5 +4,6 @@ export const ANIMAL_RADIUS = 4;
 export const WIDTH_CANVAS = 800;
 export const HEIGHT_CANVAS = 800;
 export const LIMIT_DATA_AMOUNT_FOR_NEW_REQUEST = 100000;
-export const FILE_PATH_IMAGE_TERRITORY = "/map.png";
 export const TIME_RANGE_FOR_STATISTICS_IN_STEPS = 24;
+export const FILE_PATH_IMAGE_TERRITORY = "/map.png";
+export const FILE_PATH_IMAGE_SIGNIN_REGISTER = "/screen.jpg";
