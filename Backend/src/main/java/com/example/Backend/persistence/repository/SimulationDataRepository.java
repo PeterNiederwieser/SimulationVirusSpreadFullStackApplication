80fd6b161163683a8c5728a743d9f6ea0fd0bfca
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-08-01 19:00:07 +0200

    Add deleteAllBySimulationId in SimulationDataRepository

--- Backend/src/main/java/com/example/Backend/persistence/repository/SimulationDataRepository.java
+++ Backend/src/main/java/com/example/Backend/persistence/repository/SimulationDataRepository.java
@@ -2,12 +2,11 @@ package com.example.Backend.persistence.repository;
 
 import com.example.Backend.persistence.entity.SimulationData;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface SimulationDataRepository extends JpaRepository<SimulationData, Long> {
     List<SimulationData> findBySimulationIdAndStepNumberBetween(long simulationId, int stepNumberFloor, int stepNumberCeil);
     boolean existsByStepNumberAndSimulationId(int stepNumber, long simulationId);
+    void deleteAllBySimulationId(long simulationId);
 }
