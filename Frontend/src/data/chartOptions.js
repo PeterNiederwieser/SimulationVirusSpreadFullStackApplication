e5f2a3e6a494d31d90a09a73dc93f1f935948fe1
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-08-01 18:55:11 +0200

    Add chartOptions.js

---
+++ Frontend/src/data/chartOptions.js
@@ -0,0 +1,33 @@
+export const optionsPieChart = {
+    title: "",
+    height: 300,
+    width: 550,
+    colors: ["#38f5f5", "#f5e616", "#fa602d", "#7F00FF", "#000000"],
+    is3D: true,
+    curveType: "function",
+    legend: {position: "right"},
+};
+
+export const optionsLineChart = {
+    title: "New Infections / Deaths per time",
+    height: 450,
+    width: 250,
+    colors: ["#fa602d", "#000000"],
+    legend: {position: "bottom"},
+    curveType: "function",
+    vAxis: {minValue: 10, title: "number", titleTextStyle: {color: "#333"}},
+    hAxis: {minValue: 0, title: "time", titleTextStyle: {color: "#333"}},
+    chartArea: {width: "70%", height: "70%"}
+};
+
+export const optionsAreaChart = {
+    title: "Total infections / deaths",
+    height: 450,
+    width: 250,
+    colors: ["#fa602d", "#000000"],
+    legend: {position: "bottom"},
+    curveType: "function",
+    vAxis: {minValue: 0, title: "number", titleTextStyle: {color: "#333"}},
+    hAxis: {minValue: 0, title: "time", titleTextStyle: {color: "#333"}},
+    chartArea: {width: "70%", height: "70%"}
+};
