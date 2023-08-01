92b8894bbdf7e7a804e61b3f1e67f9463280c204
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-08-01 18:55:29 +0200

    Add overviewItem.js

---
+++ Frontend/src/service/overviewItem.js
@@ -0,0 +1,30 @@
+import {
+    deleteSimulationBasicDataById,
+    getAllSimulationsBasicData,
+    updateSimulationBasicData
+} from "./requestMethods.js";
+
+export function updateItem(formObject, setSimulationsBasicData) {
+    updateSimulationBasicData(formObject)
+        .then(() => {
+            getAllSimulationsBasicData()
+                .then(data => {
+                    console.log("data after update: ", data);
+                    setSimulationsBasicData(data);
+                });
+        })
+        .catch(error => {
+            throw error;
+        });
+}
+
+export function deleteItem(item, setSimulationsBasicData) {
+    deleteSimulationBasicDataById(item.id)
+        .then(() => {
+            getAllSimulationsBasicData()
+                .then(data => setSimulationsBasicData(data));
+        })
+        .catch(error => {
+            throw error;
+        });
+}
