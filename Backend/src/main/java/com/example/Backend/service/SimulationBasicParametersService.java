66f0f405c2e3dc804220686d123e2ca962de02a1
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-08-01 19:03:24 +0200

    Refactor SimulationBasicParametersService

--- Backend/src/main/java/com/example/Backend/service/SimulationBasicParametersService.java
+++ Backend/src/main/java/com/example/Backend/service/SimulationBasicParametersService.java
@@ -1,33 +1,29 @@
 package com.example.Backend.service;
 
-import com.example.Backend.configuration.ConfigurationConstants;
 import com.example.Backend.persistence.entity.SimulationBasicParameters;
 import com.example.Backend.persistence.repository.SimulationBasicParametersRepository;
 import com.example.Backend.security.data.User;
 import com.example.Backend.security.data.UserRepository;
-import com.example.Backend.data.Context;
-import com.example.Backend.service.simulation.logic.initialisation.Initializer;
+import com.example.Backend.service.utils.SimulationBasicParametersUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
-import java.util.Random;
 
 @Service
 public class SimulationBasicParametersService {
     private final SimulationBasicParametersRepository simulationBasicParametersRepository;
+    private final SimulationDataService simulationDataService;
     private final UserRepository userRepository;
-    private final SimulationContextStorage simulationContextStorage;
-    private final Initializer initializer;
-    private final ConfigurationConstants configurationConstants;
+    private final SimulationBasicParametersUtils simulationBasicParametersUtils;
 
-    public SimulationBasicParametersService(SimulationBasicParametersRepository simulationBasicParametersRepository, UserRepository userRepository, SimulationContextStorage simulationContextStorage, Initializer initializer, ConfigurationConstants configurationConstants) {
+    public SimulationBasicParametersService(SimulationBasicParametersRepository simulationBasicParametersRepository, SimulationDataService simulationDataService, UserRepository userRepository, SimulationBasicParametersUtils simulationBasicParametersUtils) {
         this.simulationBasicParametersRepository = simulationBasicParametersRepository;
+        this.simulationDataService = simulationDataService;
         this.userRepository = userRepository;
-        this.simulationContextStorage = simulationContextStorage;
-        this.initializer = initializer;
-        this.configurationConstants = configurationConstants;
+        this.simulationBasicParametersUtils = simulationBasicParametersUtils;
     }
 
     public List<SimulationBasicParameters> findAllByUser(String userEmail) {
@@ -37,39 +33,19 @@ public class SimulationBasicParametersService {
     public Optional<SimulationBasicParameters> findById(long id) {
         return simulationBasicParametersRepository.findById(id);
     }
-
+    @Transactional
     public SimulationBasicParameters create(SimulationBasicParameters simulationBasicParameters, String userEmail) throws IOException {
         User user = findUserByEmail(userEmail);
         simulationBasicParameters.setUser(user);
-        addRandomSeed(simulationBasicParameters);
+        simulationBasicParameters.setSeed(simulationBasicParametersUtils.getRandomSeed());
         SimulationBasicParameters persisted = simulationBasicParametersRepository.save(simulationBasicParameters);
-        setupSimulation(persisted);
-        return persisted;
-    }
-
-    private void addRandomSeed(SimulationBasicParameters simulationBasicParameters) {
-        int seed = (int) Math.round(Math.random() * configurationConstants.getSeedLimit());
-        simulationBasicParameters.setSeed(seed);
-    }
-
-    private void setupSimulation(SimulationBasicParameters persisted) throws IOException {
-        Random random = getRandomWithSeed(persisted);
-        Context context = new Context(persisted.getId(), persisted.getNumberOfAnimals(), persisted.getNumberOfInitialInfections(), random);
-        initializer.initializeSimulation(context);
-        simulationContextStorage.addContext(persisted.getId(), context);
-    }
-
-    private Random getRandomWithSeed(SimulationBasicParameters persisted) {
-        int seed = persisted.getSeed();
-        return new Random(seed);
+        simulationDataService.deleteAllBySimulationId(persisted.getId());
+        simulationBasicParametersUtils.setupSimulation(persisted);
+        return simulationBasicParameters;
     }
 
     private User findUserByEmail(String userEmail) {
         return userRepository.findByEmail(userEmail).get();
-    }
-
-    public SimulationBasicParameters update(SimulationBasicParameters simulationBasicParameters) {
-        return simulationBasicParametersRepository.save(simulationBasicParameters);
     }
 
     public void deleteById(long id) {
