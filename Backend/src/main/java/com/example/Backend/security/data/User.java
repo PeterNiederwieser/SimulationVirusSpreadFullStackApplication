d88401d10209743da52178cb72239f23f40e6f6d
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-08-05 14:36:06 +0200

    Add @Builder to User entity

--- Backend/src/main/java/com/example/Backend/security/data/User.java
+++ Backend/src/main/java/com/example/Backend/security/data/User.java
@@ -2,6 +2,7 @@ package com.example.Backend.security.data;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import java.util.Set;
 @Entity
 @Table(name = "_user")
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
